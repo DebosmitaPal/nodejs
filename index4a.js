d0823129cc99780389cb031284ba88579a9b2834
DebosmitaPal <debosmitapal429@gmail.com>
2026-02-19 13:45:05 +0530

    modified

--- index4a.js
+++ index4a.js
@@ -8,3 +8,9 @@ app.get("/:a/:b",(req,res)=>{
     let b=parseInt(req.params.b)
     res.send((a+b).toString())
 })
+app.get("/x/:x/y/:y/z/:z",(req,res)=>{
+    let x=parseInt(req.params.x)
+    let y=parseInt(req.params.y)
+    let z=parseInt(req.params.z)
+    res.send((x+y+z).toString())
+})
